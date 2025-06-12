c191d87f731fab669d8d516d5bb3871829da97a2
Trelawney Melani <trelawnm@ne-f4.nn.21-school.ru>
2025-06-12 16:21:09 +0300

    feature(strerror): macros definded array of errors. main logic added

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,26 +1,27 @@
 #include "../headers/s21_errno.h"
 #include "../headers/s21_string.h"
 
-const char *const errlist[] =
-  {
-#define _S(n, str)         [n] = str,
-#include "../headers/s21_errlist.h"
-#undef _S
-  };
-const s21_size_t errlist_len = sizeof (errlist) / sizeof ((errlist)[0]);
-
+#include <string.h>
 
 /**
  * @brief get error message string
- * @return a pointer to a string that describes the error code passed in  the  argument  errnum
- * 
+ * @return a pointer to a string that describes the error code passed in  the
+ * argument  errnum
+ *
  * @version 1.0
  * @date June 6, 2025
  * @author Demian Domozhirov (DarkDomian/trelawnm)
  */
 char *s21_strerror(int errnum) {
-    if (errnum >= 0 && errnum < errlist_len)
-        return errlist[errnum];
-    else 
-        return (char *)"Unknown error";
+  const char *const errlist[] = {
+#define _S(n, str) [n] = str,
+#include "../headers/s21_errlist.h"
+#undef _S
+  };
+
+  if (errnum >= 0 && errnum < S21_ERRlIST_LEN && errlist[errnum] != S21_NULL)
+    return (char *)errlist[errnum];
+  else
+    return (char *)"Unknown error ";  // TODO: add to the returned message value
+                                      // of errnum
 }
