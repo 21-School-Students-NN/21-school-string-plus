4777cb9f6cec2df8ee04f242df0a8e91218de28f
Trelawney Melani <trelawnm@ne-f4.nn.21-school.ru>
2025-06-12 16:26:43 +0300

    tests(strerror): add testing for strerror. start work on test/main.c - the inter point for testing

---
+++ tests/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <check.h>
+
+// #include "../headers/s21_string.h"
+
+// #include "./test_strerror.c"
+
+int main(void) {
+    int number_failed;
+    Suite *s = strerror_suite();
+    SRunner *sr = srunner_create(s);
+
+    srunner_set_log (sr, "test.log");
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
--- tests/test_strerror.c
+++ tests/test_strerror.c
@@ -1,1 +1,51 @@
 #include <check.h>
+#include <errno.h>
+#include <string.h>
+
+#include "../headers/s21_string.h"
+#include "../headers/s21_errno.h"
+
+
+START_TEST(test_strerror_known_errors) {
+    // Проверяем известные коды ошибок
+    ck_assert_str_eq(s21_strerror(0), strerror(0));
+    ck_assert_str_eq(s21_strerror(1), strerror(1));
+    ck_assert_str_eq(s21_strerror(2), strerror(2));
+    ck_assert_str_eq(s21_strerror(ENOENT), strerror(ENOENT));
+    ck_assert_str_eq(s21_strerror(EACCES), strerror(EACCES));
+}
+END_TEST
+
+START_TEST(test_strerror_unknown_errors) {
+    // Проверяем неизвестные коды ошибок (отрицательные и большие значения)
+    ck_assert_str_eq(s21_strerror(-1), strerror(-1));
+    ck_assert_str_eq(s21_strerror(9999), strerror(9999));
+    ck_assert_str_eq(s21_strerror(S21_ERRlIST_LEN + 1), strerror(S21_ERRlIST_LEN + 1));
+}
+END_TEST
+
+START_TEST(test_strerror_integration) {
+    // Проверяем все известные ошибки в цикле
+    for (int err = 0; err < S21_ERRlIST_LEN; err++) {
+        const char *sys_str = strerror(err);
+        const char *our_str = s21_strerror(err);
+        ck_assert_msg(strcmp(sys_str, our_str) == 0,
+                     "Error %d: system='%s', our='%s'", err, sys_str, our_str);
+    }
+}
+END_TEST
+
+Suite *strerror_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("strerror");
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_strerror_known_errors);
+    tcase_add_test(tc_core, test_strerror_unknown_errors);
+    tcase_add_test(tc_core, test_strerror_integration);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
