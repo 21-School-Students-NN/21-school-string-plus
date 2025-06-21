4df50cfaf4aeee2f53401ade4648d0a0bc1461b4
Demian Domozhirov <darkdomian@gmail.com>
2025-06-21 14:13:55 +0300

    fix(style): fixing style in s21_strcspn and test_strcspn.c

--- src/s21_strcspn.c
+++ src/s21_strcspn.c
@@ -1,18 +1,18 @@
 #include "../headers/s21_string.h"
 
 s21_size_t s21_strcspn(const char *str1, const char *str2) {
-    const char *s; // from `source`
-    const char *r; // from reject
-    s21_size_t count = 0;
-  
-    for (s = str1; *s != '\0'; ++s) {
-      for (r = str2; *r != '\0'; ++r)
-        if (*s == *r) break;
-      if (*r == '\0')
-        ++count;
-      else
-        return count;
-    }
-  
-    return count;
+  const char *s;  // from `source`
+  const char *r;  // from reject
+  s21_size_t count = 0;
+
+  for (s = str1; *s != '\0'; ++s) {
+    for (r = str2; *r != '\0'; ++r)
+      if (*s == *r) break;
+    if (*r == '\0')
+      ++count;
+    else
+      return count;
+  }
+
+  return count;
 }
