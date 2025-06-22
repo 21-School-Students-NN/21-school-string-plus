fddd445026d8569fac9117902ff50b230fb906b9
Trelawney Melani <trelawnm@ne-f4.nn.21-school.ru>
2025-06-22 17:03:56 +0300

    feature(strncmp): final version - find and solve edge case, when first string rich the '\0' we should execute the subtraction - normilize output from original strncmp() for testing with  function

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,24 +1,13 @@
 #include "../headers/s21_string.h"
 
-#include <valgrind/valgrind.h>
-
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
-    int res = 0;
-    while (n != 0 && *str1 != '\0' && *str2 != '\0') {
-        if (*str1 != *str2) {
-            if (RUNNING_ON_VALGRIND)
-                res = (*str1 < *str2) ? -1 : 1;
-            else
-                res = *str1 - *str2;
-            break;
-        }
-        
-        if (n != 1) {
-            ++str1;
-            ++str2;
-        }
-        --n;
-    }
-    
-    return res;
+  unsigned char u1, u2;
+
+  while (n--) {
+    u1 = (unsigned char)*str1++;
+    u2 = (unsigned char)*str2++;
+    if (u1 != u2) return u1 - u2;
+    if (u1 == '\0') return 0;
+  }
+  return 0;
 }
