999af0489eae530d8c295395cd2766d2d36543c7
Trelawney Melani <trelawnm@sh-h7.nn.21-school.ru>
2025-06-25 18:45:51 +0300

    fix(style): combine declaration and inicialization. no affect on performance (probably)

--- src/s21_strncmp.c
+++ src/s21_strncmp.c
@@ -1,11 +1,9 @@
 #include "../headers/s21_string.h"
 
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
-  unsigned char u1, u2;
-
   while (n--) {
-    u1 = (unsigned char)*str1++;
-    u2 = (unsigned char)*str2++;
+    unsigned char u1 = (unsigned char)*str1++;
+    unsigned char u2 = (unsigned char)*str2++;
     if (u1 != u2) return u1 - u2;
     if (u1 == '\0') return 0;
   }
