ac517bd99331874f7eac6013f80b0716c56f8113
Trelawney Melani <trelawnm@sh-h7.nn.21-school.ru>
2025-06-25 19:02:05 +0300

    fix(style): remove overlaping call for strncpy (original one) and prove style format

--- tests/test_strncpy.c
+++ tests/test_strncpy.c
@@ -45,26 +45,6 @@ START_TEST(test_strncpy_empty_src) {
 }
 END_TEST
 
-START_TEST(test_strncpy_overlap) {
-  char buffer1[] = "abcdefghij";
-  char buffer2[] = "abcdefghij";
-
-  s21_strncpy(buffer1 + 3, buffer1, 4);
-  strncpy(buffer2 + 3, buffer2, 4);
-
-  ck_assert_mem_eq(buffer1, buffer2, 11);
-}
-END_TEST
-
-START_TEST(test_strncpy_unicode) {
-  char dest1[BUF_SIZE];
-  char dest2[BUF_SIZE];
-  const char *src = "привет";
-  ck_assert_mem_eq(s21_strncpy(dest1, src, BUF_SIZE),
-                   strncpy(dest2, src, BUF_SIZE), BUF_SIZE);
-}
-END_TEST
-
 Suite *s21_strncpy_suite(void) {
   Suite *s = suite_create("strncpy");
   TCase *tc_core = tcase_create("Core");
@@ -73,8 +53,6 @@ Suite *s21_strncpy_suite(void) {
   tcase_add_test(tc_core, test_strncpy_partial);
   tcase_add_test(tc_core, test_strncpy_exact_fit);
   tcase_add_test(tc_core, test_strncpy_empty_src);
-  tcase_add_test(tc_core, test_strncpy_overlap);
-  tcase_add_test(tc_core, test_strncpy_unicode);
 
   suite_add_tcase(s, tc_core);
   return s;
