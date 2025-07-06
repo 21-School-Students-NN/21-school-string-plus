43e9b0320097b107826e86a22b458f0f72665be1
murk13hack <min-mar-i@yandex.ru>
2025-07-07 00:09:37 +0300

    added comments in English to strstr.c

--- src/s21_strstr.c
+++ src/s21_strstr.c
@@ -1,32 +1,33 @@
 #include "../headers/s21_string.h"
-
 char *s21_strstr(const char *haystack, const char *needle) {
-  if (needle == S21_NULL || haystack == S21_NULL) {
+    // if any of the strings points to null, then we return null
+    if (needle == S21_NULL || haystack == S21_NULL) {
+        return S21_NULL;
+    }
+
+    // if the needle is empty, we return the entire haystack.
+    if (*needle == '\0') {
+        return (char *)haystack;
+    }
+
+    // walking through a haystack
+    for (; *haystack; haystack++) {
+        // initialization of constant pointers to the first elements of the needle and haystack
+        const char *h = haystack;
+        const char *n = needle;
+
+        // the characters are compared until the needle runs out or there is no discrepancy
+        while (*h && *n && (*h == *n)) {
+            h++;
+            n++;
+        }
+
+        // if the end of the needle has been reached, then the first occurrence of the substring has been found
+        if (*n == '\0') {
+            return (char *)haystack;
+        }
+    }
+
+    // the needle wasn't in the haystack
     return S21_NULL;
-  }
-
-  // Если needle пустая строка, вернём haystack (по стандарту)
-  if (*needle == '\0') {
-    return (char *)haystack;
-  }
-
-  // Проходим по haystack
-  for (; *haystack; haystack++) {
-    const char *h = haystack;
-    const char *n = needle;
-
-    // Сравниваем символы, пока не кончится needle или не будет несоответствия
-    while (*h && *n && (*h == *n)) {
-      h++;
-      n++;
-    }
-
-    // Если дошли до конца needle — значит, нашли подстроку
-    if (*n == '\0') {
-      return (char *)haystack;
-    }
-  }
-
-  // Не нашли
-  return S21_NULL;
 }
