26bb77ab990ace2bb853d6c4068ed8b58f32ba7a
murk13hack <min-mar-i@yandex.ru>
2025-07-15 22:15:33 +0300

    deleted the extra tests

--- tests/test_strstr.c
+++ tests/test_strstr.c
@@ -1,44 +1,17 @@
 #include "../headers/s21_string.h"
 #include "./suites.h"
 
-// custom solution test for substring search
-START_TEST(test_strstr_found) {
-  const char *haystack = "Hello, world!";
-  const char *needle = "world";
-  char *result = s21_strstr(haystack, needle);
-  ck_assert_ptr_nonnull(result);
-  ck_assert_str_eq(result, "world!");
-}
-END_TEST
+/* comparative tests */
 
-// custom solution test for missing substring
-START_TEST(test_strstr_not_found) {
-  const char *haystack = "Hello, world!";
-  const char *needle = "foo";
-  char *result = s21_strstr(haystack, needle);
-  ck_assert_ptr_null(result);
-}
-END_TEST
-
-// custom solution test for empty needle
-START_TEST(test_strstr_empty_needle) {
-  const char *haystack = "Hello, world!";
-  const char *needle = "";
-  char *result = s21_strstr(haystack, needle);
-  ck_assert_ptr_eq(result, haystack);
-}
-END_TEST
-
-// custom solution test for needle = haystack
+// needle = haystack
 START_TEST(test_strstr_needle_eq_haystack) {
   const char *haystack = "Hello";
   const char *needle = "Hello";
-  char *result = s21_strstr(haystack, needle);
-  ck_assert_ptr_eq(result, haystack);
+  const char *original_result = strstr(haystack, needle);
+  const char *custom_result = s21_strstr(haystack, needle);
+  ck_assert_ptr_eq(original_result, custom_result);
 }
 END_TEST
-
-/* comparative tests */
 
 // finding an existing substring
 START_TEST(test_strstr_basic_match) {
@@ -136,9 +109,6 @@ Suite *s21_strstr_suite(void) {
   TCase *tc = tcase_create("Core");
 
   // add tests to test-case
-  tcase_add_test(tc, test_strstr_found);
-  tcase_add_test(tc, test_strstr_not_found);
-  tcase_add_test(tc, test_strstr_empty_needle);
   tcase_add_test(tc, test_strstr_needle_eq_haystack);
   tcase_add_test(tc, test_strstr_basic_match);
   tcase_add_test(tc, test_strstr_multiple_matches);
