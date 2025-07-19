e1d04aa75ccdaffa32fbf7f56f1d40fa8ccf21b8
murk13hack <min-mar-i@yandex.ru>
2025-07-19 17:20:39 +0300

    after refactoring (-test_sscanf_n_specifier and -test_sscanf_mixed_types)

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -1,6 +1,8 @@
 #include "../headers/s21_sscanf.h"
 
 #include "../headers/s21_string.h"
+#include "s21_strlen.c"
+#include "s21_strncmp.c"
 
 /**
  * there is a comment about table work
@@ -18,104 +20,152 @@ static int parse_count(const char **, const fmt_token_t *, va_list *);
  * different functions worked outside from table
  */
 static void init_fmt_token(fmt_token_t *tok);
-static int format_parsing(const char **format, fmt_token_t *tok);
+static int parse_format(const char **format, const char **str, va_list *args);
+static int function_for_specifier(const char **str,
+                                  const dispatch_entry_t *dispatch_table,
+                                  const int d_t_size, fmt_token_t *tok,
+                                  va_list *args);
+static void space_skip(const char **format, const char **str);
+static int processing_specifiers(const char **format, fmt_token_t *tok,
+                                 const int length_modifier_t_size,
+                                 const dispatch_length_modifier *lmt);
+static int skip_before_fspec(const char **format, const char **str);
 static int char_to_digit(char c, int base);
 
 int s21_sscanf(const char *str, const char *format, ...) {
   if (str == S21_NULL || format == S21_NULL || *str == '\0') {
     return S21_EOF;
   }
+  va_list args;
+  va_start(args, format);
+  int res = parse_format(&format, &str, &args);
+  va_end(args);
+  return res;
+}
+static int parse_format(const char **format, const char **str, va_list *args) {
+  fmt_token_t tok;
+  init_fmt_token(&tok);
   dispatch_entry_t dispatch_table[] = {
       {'c', parse_char},   {'d', parse_int},     {'i', parse_int},
       {'e', parse_float},  {'E', parse_float},   {'f', parse_float},
       {'g', parse_float},  {'G', parse_float},   {'o', parse_octal},
       {'s', parse_string}, {'u', parse_uint},    {'x', parse_uint},
       {'X', parse_uint},   {'p', parse_pointer}, {'n', parse_count}};
+
+  dispatch_length_modifier lmt[] = {{"h", LEN_H},  {"hh", LEN_HH},
+                                    {"l", LEN_L},  {"ll", LEN_LL},
+                                    {"L", LEN_LD}, {"", LEN_NONE}};
+  int flag = 1;
+  int status_proc_spec = 0;
+  int counter = 0;
+  const char *fp = *format;
+  const char *strp = *str;
+  tok.start_ptr = strp;
   const int dispatch_table_size =
       sizeof(dispatch_table) / sizeof(dispatch_table[0]);
-  va_list args;
-  va_start(args, format);
-  const char **str_ptr = &str;
-  const char **format_ptr = &format;
-  fmt_token_t tok;
-  init_fmt_token(&tok);
-  tok.start_ptr = str;
-  int counter = 0, flag = 1;
-  while (flag && format_parsing(format_ptr, &tok)) {
-    while (isspace((unsigned char)**str_ptr)) (*str_ptr)++;
-    if (tok.specifier && tok.specifier != '%') {
-      for (int i = 0; i < dispatch_table_size; i++) {
-        if (dispatch_table[i].specifier == tok.specifier) {
-          int res = dispatch_table[i].func(str_ptr, &tok, &args);
-          if (res == 0 && !tok.suppress) {
-            flag = 0;
-          } else {
-            counter += res;
-          }
-        }
-      }
-    } else if (tok.literal || tok.specifier == '%') {
-      if (**str_ptr == tok.literal) {
-        (*str_ptr)++;
-      } else {
+  const int length_modifier_t_size = sizeof(lmt) / sizeof(lmt[0]);
+  while (flag) {
+    space_skip(&fp, &strp);
+    int whats_next = skip_before_fspec(&fp, &strp);
+    if (whats_next == 2) {
+      status_proc_spec =
+          processing_specifiers(&fp, &tok, length_modifier_t_size, lmt);
+    } else {
+      flag = 0;
+    }
+    if (flag && status_proc_spec) {
+      int res = function_for_specifier(&strp, dispatch_table,
+                                       dispatch_table_size, &tok, args);
+      if (res == 0 && !tok.suppress) {
         flag = 0;
+      } else {
+        counter += res;
       }
     }
   }
-  va_end(args);
   return counter;
 }
-
-static int format_parsing(const char **format, fmt_token_t *tok) {
-  init_fmt_token(tok);
-  const char *p = *format;
-  int success = 1;
-  while (*p == ' ' || *p == '\t' || *p == '\n') p++;
-  if (*p == '\0') {
-    success = 0;
-  } else if (*p == '%') {
-    p++;
-    if (*p == '%') {
-      tok->literal = '%';
-      p++;
-    } else {
-      if (*p == '*') {
-        tok->suppress = 1;
-        p++;
-      }
-      while (isdigit((unsigned char)*p)) {
-        tok->width = tok->width * 10 + (*p - '0');
-        p++;
-      }
-      if (p[0] == 'h' && p[1] == 'h') {
-        tok->length_modifier = LEN_HH;
-        p += 2;
-      } else if (p[0] == 'h') {
-        tok->length_modifier = LEN_H;
-        p++;
-      } else if (p[0] == 'l' && p[1] == 'l') {
-        tok->length_modifier = LEN_LL;
-        p += 2;
-      } else if (p[0] == 'l') {
-        tok->length_modifier = LEN_L;
-        p++;
-      } else if (p[0] == 'L') {
-        tok->length_modifier = LEN_LD;
-        p++;
-      }
-      if (*p != '\0' && s21_strchr("diouxXfFeEgGaAcspn", *p)) {
-        tok->specifier = *p;
-        p++;
-      } else {
-        success = 0;
-      }
+static int function_for_specifier(const char **str,
+                                  const dispatch_entry_t *dispatch_table,
+                                  const int d_t_size, fmt_token_t *tok,
+                                  va_list *args) {
+  int res = 0;
+  for (int i = 0; i < d_t_size; i++) {
+    if (dispatch_table[i].specifier == tok->specifier) {
+      res = dispatch_table[i].func(str, tok, args);
     }
-  } else {
-    tok->literal = *p;
-    p++;
   }
-  *format = p;
+  return res;
+}
+static void space_skip(const char **format, const char **str) {
+  const char *fp = *format;
+  const char *strp = *str;
+  if (isspace((unsigned char)*fp)) {
+    while (isspace((unsigned char)*fp)) fp++;
+    while (isspace((unsigned char)*strp)) strp++;
+  }
+  *format = fp;
+  *str = strp;
+}
+
+static int processing_specifiers(const char **format, fmt_token_t *tok,
+                                 const int lmts,
+                                 const dispatch_length_modifier *lmt) {
+  init_fmt_token(tok);
+  const char *fp = *format;
+  int success = 1;
+  if (*fp == '*') {
+    tok->suppress = 1;
+    fp++;
+  }
+  while (isdigit((unsigned char)*fp)) {
+    tok->width = tok->width * 10 + (*fp - '0');
+    fp++;
+  }
+  for (int i = 0; i < lmts; i++) {
+    if (s21_strncmp(fp, lmt[i].clm, s21_strlen(lmt[i].clm)) == 0) {
+      tok->length_modifier = lmt[i].length_modifier;
+      fp += s21_strlen(lmt[i].clm);
+      break;
+    }
+  }
+  if (*fp != '\0' && s21_strchr("diouxXfFeEgGaAcspn", *fp)) {
+    tok->specifier = *fp;
+    fp++;
+  } else {
+    success = 0;
+  }
+  *format = fp;
   return success;
+}
+static int skip_before_fspec(const char **format, const char **str) {
+  int fl = 0;  // discrepancy=1, start_fspec=2, the end=0.
+  const char *fp = *format;
+  const char *strp = *str;
+  while (*fp && *strp) {
+    if (*fp == '%' && *(fp + 1) != '%') {
+      fp++;
+      fl = 2;
+      break;
+    } else if (*fp == '%' && *(fp + 1) == '%') {
+      if (*strp == '%') {
+        fp += 2;
+        strp++;
+      } else {
+        fl = 1;
+        break;
+      }
+    } else if (*fp == *strp) {
+      fp++;
+      strp++;
+    } else {
+      fl = 1;
+      break;
+    }
+  }
+  *format = fp;
+  *str = strp;
+  return fl;
 }
 
 static void init_fmt_token(fmt_token_t *tok) {
@@ -409,7 +459,7 @@ static int char_to_digit(char c, int base) {
   }
   return digit;
 }
-
+#include <stdio.h>
 static int parse_count(const char **str, const fmt_token_t *tok,
                        va_list *args) {
   if (!tok->suppress) {
