4cd9223114493320ce30de68e74d70e4030e83c6
Trelawney Melani <trelawnm@sh-g2.nn.21-school.ru>
2025-07-21 19:59:48 +0300

    final cut

--- src/s21_strerror.c
+++ src/s21_strerror.c
@@ -1,9 +1,6 @@
 #include "../headers/s21_errno.h"
 #include "../headers/s21_string.h"
 
-// TODO: remove stdio.h
-#include <stdio.h>
-
 char *s21_strerror(int errnum) {
   const char *const errlist[] = {
 #define _S(n, str) [n] = str,
@@ -16,8 +13,7 @@ char *s21_strerror(int errnum) {
   if (errnum >= 0 && errnum <= S21_ERRLIST_LEN && errlist[errnum] != S21_NULL)
     return (char *)errlist[errnum];
   else {
-    // TODO: change to s21_sprintf
-    sprintf(errmes, "Unknown error %d", errnum);
+    s21_sprintf(errmes, "Unknown error %d", errnum);
   }
   return errmes;
 }
