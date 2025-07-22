22764f6ac7f65e18c2304e86047a947a4111d06a
Rhydon Terwilliger <rhydonte@ne-g1.nn.21-school.ru>
2025-07-22 10:00:47 +0300

    Style fix

--- tests/test_strtok.c
+++ tests/test_strtok.c
@@ -4,11 +4,11 @@
 #include "../headers/s21_string.h"
 #include "./suites.h"
 
-#define ALICE_TEXT                                                                 \
-  "Alice was beginning to get very tired of sitting by her sister on the "         \
-  "bank, and of having nothing to do: once or twice she had peeped into the "      \
-  "book her sister was reading, but it had no pictures or conversations in "       \
-  "it, “and what is the use of a book,” thought Alice “without pictures or " \
+#define ALICE_TEXT                                                            \
+  "Alice was beginning to get very tired of sitting by her sister on the "    \
+  "bank, and of having nothing to do: once or twice she had peeped into the " \
+  "book her sister was reading, but it had no pictures or conversations in "  \
+  "it, “and what is the use of a book,” thought Alice “without pictures or "  \
   "conversations?”"
 
 #define ALICE_SIZE 384
